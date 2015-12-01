bc88ae588a0002e130985a3ae1c8b18b1907f167
Josh Komie <joshkomie@gmail.com>
2015-11-30 21:26:48 -0800

    initial

---
+++ app/views/talents/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @talent, :id, :First, :Last, :Description, :done, :created_at, :updated_at
---
+++ db/migrate/20151125001200_create_talents.rb
@@ -0,0 +1,12 @@
+class CreateTalents < ActiveRecord::Migration
+  def change
+    create_table :talents do |t|
+      t.string :First
+      t.string :Last
+      t.text :Description
+      t.boolean :done
+
+      t.timestamps null: false
+    end
+  end
+end
